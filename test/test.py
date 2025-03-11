369d73c2e7c92dbc017425ddd2b9cb7947e6c884
yu xiong <xiongyu@espressif.com>
2025-03-11 14:37:02 +0800

    update test.py, add more test

--- test/test.py
+++ test/test.py
@@ -4,7 +4,8 @@ import os
 # 将项目根目录添加到系统路径
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import parse_args
-from web import Paginator
+from web import Paginator, WebApp
+import argparse
 
 class TestConfig(unittest.TestCase):
     def test_parse_args(self):
@@ -20,6 +21,48 @@ class TestPaginator(unittest.TestCase):
         per_page = 10
         result, total_pages = Paginator.paginate(items, page, per_page)
         self.assertEqual(len(result), per_page)
+
+    def test_paginate_empty_list(self):
+        items = []
+        page = 1
+        per_page = 10
+        result, total_pages = Paginator.paginate(items, page, per_page)
+        self.assertEqual(len(result), 0)
+        self.assertEqual(total_pages, 1)
+
+class TestWebApp(unittest.TestCase):
+    def setUp(self):
+        # 创建一个模拟的参数对象
+        args = argparse.Namespace(
+            per_page=20,
+            host='0.0.0.0',
+            port=5000,
+            input_json='test.json',
+            replace=[('old', 'new')]
+        )
+        self.web_app = WebApp(args)
+
+    def test_apply_replace_rules(self):
+        data = {'key': 'old value'}
+        result = self.web_app.apply_replace_rules(data)
+        self.assertEqual(result['key'], 'new value')
+
+    def test_reverse_replace_rules(self):
+        data = {'key': 'new value'}
+        result = self.web_app.reverse_replace_rules(data)
+        self.assertEqual(result['key'], 'old value')
+
+    def test_apply_replace_rules_no_rules(self):
+        self.web_app.replace_rules = []
+        data = {'key': 'old value'}
+        result = self.web_app.apply_replace_rules(data)
+        self.assertEqual(result, data)
+
+    def test_reverse_replace_rules_no_rules(self):
+        self.web_app.replace_rules = []
+        data = {'key': 'old value'}
+        result = self.web_app.reverse_replace_rules(data)
+        self.assertEqual(result, data)
 
 if __name__ == '__main__':
     unittest.main()
